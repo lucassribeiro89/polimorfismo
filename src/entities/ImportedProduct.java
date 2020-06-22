7b18f3d3bfc32ea7caa5df51ffca3269816621ba
Lucas Silvestre <lucassribeiro.adv@gmail.com>
2020-06-22 14:00:46 -0300

    projeto de produtos

---
+++ src/entities/ImportedProduct.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class ImportedProduct extends Product{
+
+	protected Double customsFee;
+
+	public ImportedProduct(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+
+	public Double getCustomsFee() {
+		return customsFee;
+	}
+
+	public void setCustomsFee(Double customsFee) {
+		this.customsFee = customsFee;
+	}
+	
+	public double totalPrice() {
+		return price + customsFee;
+	}
+
+	@Override
+	public String priceTag() {
+		return super.priceTag() + " (Customs fee: " + String.format("%.2f", totalPrice()) + ")";
+	}
+}
